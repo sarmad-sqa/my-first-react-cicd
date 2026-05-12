2bf16968951880c9397f30bd1cd4a140f550d019
Sarmad Sarwar <sarmadsqa01@gmail.com>
2026-05-12 20:45:16 +0500

    new changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('check if submit button is present', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // "Submit" button dhoond raha hai
+  const buttonElement = screen.getByText(/submit/i); 
+  expect(buttonElement).toBeInTheDocument();
 });
